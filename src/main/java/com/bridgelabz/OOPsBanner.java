577559bcbb8bcbea8c6095b394e3456753a715ab
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-01-29 21:37:41 +0530

    coded refactoring Banner logic into function

--- src/main/java/com/bridgelabz/OOPsBanner.java
+++ src/main/java/com/bridgelabz/OOPsBanner.java
@@ -1,4 +1,58 @@
 package com.bridgelabz;
 
+/**
+ *
+ *
+ * @author Sumukha
+ * @version 5.0
+ */
+
 public class OOPsBanner {
+    public static String[] getOPattern(){
+        return new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        };
+    }
+
+    public static String[] getPPattern(){
+        return new String[]{
+                "******   ",
+                "**    **",
+                "**    ** ",
+                "*******  ",
+                "**       ",
+                "**       ",
+                "**       "};
+        }
+
+        public static String[] getSPattern() {
+            return new String[]{
+                    "  ****** ",
+                    "**       ",
+                    "**       ",
+                    " ******* ",
+                    "       **",
+                    "       **",
+                    " ******  "
+            };
+        }
+
+    public static void main(String[] args) {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(oPattern[i]+" "+oPattern[i]+" "+pPattern[i]+" "+sPattern[i]);
+        }
+    }
+
 }
+
