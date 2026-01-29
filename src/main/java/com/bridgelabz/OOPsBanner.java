e20e4f4163dc999079fd40f89397902d7d7168dc
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-01-29 17:24:06 +0530

    coded String.join method to optimized and clleaner code

--- src/main/java/com/bridgelabz/OOPsBanner.java
+++ src/main/java/com/bridgelabz/OOPsBanner.java
@@ -1,4 +1,20 @@
 package com.bridgelabz;
 
+/**
+ *
+ *
+ * @Author Sumukha
+ * @Version 3.0
+ */
+
 public class OOPsBanner {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ","   ***   ","    ***    ","******   ","  ****** "));
+        System.out.println(String.join(" "," **   ** ","  **   **  ","**    ** ","**       "));
+        System.out.println(String.join(" ","**     **"," **     ** ","**    ** ","**       "));
+        System.out.println(String.join(" ","**     **"," **     ** ","*******  ","******* "));
+        System.out.println(String.join(" ","**     **"," **     ** ","**       ","       **"));
+        System.out.println(String.join(" "," **   ** ","  **   **  ","**       ","       **"));
+        System.out.println(String.join(" ","   ***   ","    ***    ","**       "," ******* "));
+    }
 }
