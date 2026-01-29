886f9ff69c6290fad633575c4f3ce944759024c9
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-01-29 17:15:36 +0530

    coded multiple printing statements to display OOPS banner

--- src/main/java/com/bridgelabz/OOPsBanner.java
+++ src/main/java/com/bridgelabz/OOPsBanner.java
@@ -1,4 +1,21 @@
 package com.bridgelabz;
+/**
+ * UC2: Render OOPS Banner using Print Statements
+ *
+ *
+ * @author Sumukha
+ * @version 2.0
+ */
+
 
 public class OOPsBanner {
+    public static void main(String[] args) {
+        System.out.println("   ***       ***    ******     ****** ");
+        System.out.println(" **   **   **   **  **    ** **       ");
+        System.out.println("**     ** **     ** **    ** **       ");
+        System.out.println("**     ** **     ** *******   ******* ");
+        System.out.println("**     ** **     ** **              **");
+        System.out.println(" **   **   **   **  **              **");
+        System.out.println("   ***       ***    **        ******* ");
+    }
 }
