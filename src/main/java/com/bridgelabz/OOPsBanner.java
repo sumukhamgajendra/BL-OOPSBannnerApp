d5de0f16996a2bc2f7c8a39ca6714a3666d26297
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-01-29 17:52:42 +0530

    coded to Render OOPs banner inline array

--- src/main/java/com/bridgelabz/OOPsBanner.java
+++ src/main/java/com/bridgelabz/OOPsBanner.java
@@ -9,13 +9,13 @@ package com.bridgelabz;
 public class OOPsBanner {
     public static void main(String[] args) {
         String[] lines = {
-            String.join(" ", "   ***   ", "    ***    ", "******   ", "  ****** ");
-            String.join(" ", " **   ** ", "  **   **  ", "**    ** ", "**       ");
-            String.join(" ", "**     **", " **     ** ", "**    ** ", "**       ");
-            String.join(" ", "**     **", " **     ** ", "*******  ", "******* ");
-            String.join(" ", "**     **", " **     ** ", "**       ", "       **");
-            String.join(" ", " **   ** ", "  **   **  ", "**       ", "       **");
-            String.join(" ", "   ***   ", "    ***    ", "**       ", " ******* ");
+            String.join(" ", "   ***   ", "    ***    ", "******   ", "  ****** "),
+            String.join(" ", " **   ** ", "  **   **  ", "**    ** ", "**       "),
+            String.join(" ", "**     **", " **     ** ", "**    ** ", "**       "),
+            String.join(" ", "**     **", " **     ** ", "*******  ", "******* "),
+            String.join(" ", "**     **", " **     ** ", "**       ", "       **"),
+            String.join(" ", " **   ** ", "  **   **  ", "**       ", "       **"),
+            String.join(" ", "   ***   ", "    ***    ", "**       ", " ******* ")
         };
 
         for(String line : lines){
