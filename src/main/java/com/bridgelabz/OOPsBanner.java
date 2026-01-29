6b26e26dd32d3b350f4de707c49ce6a9541d8a71
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-01-29 17:31:08 +0530

    coded String array to add more optimization

--- src/main/java/com/bridgelabz/OOPsBanner.java
+++ src/main/java/com/bridgelabz/OOPsBanner.java
@@ -1,4 +1,28 @@
 package com.bridgelabz;
 
+/**
+ *
+ *
+ * @author Sumukha
+ * @version 4.0
+ */
+
 public class OOPsBanner {
+    public static void main(String[] args) {
+        String[] lines = new String[7];
+
+        lines[0] = String.join(" ","   ***   ","    ***    ","******   ","  ****** ");
+        lines[1] = String.join(" "," **   ** ","  **   **  ","**    ** ","**       ");
+        lines[2] = String.join(" ","**     **"," **     ** ","**    ** ","**       ");
+        lines[3] = String.join(" ","**     **"," **     ** ","*******  ","******* ");
+        lines[4] = String.join(" ","**     **"," **     ** ","**       ","       **");
+        lines[5] = String.join(" "," **   ** ","  **   **  ","**       ","       **");
+        lines[6] = String.join(" ","   ***   ","    ***    ","**       "," ******* ");
+
+        for(String line : lines){
+            System.out.println(line);
+        }
+    }
+
+
 }
