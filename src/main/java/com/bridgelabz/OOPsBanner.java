bf5ce6185df870b9270c3ad51123187c02f475d7
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-01-29 17:08:37 +0530

    Coded to print oops to console

--- src/main/java/com/bridgelabz/OOPsBanner.java
+++ src/main/java/com/bridgelabz/OOPsBanner.java
@@ -1,4 +1,16 @@
 package com.bridgelabz;
 
+/**
+ * OOPsBannerApp UC1 - OOPS Banner banner Display application
+ *
+ *
+ * @author SumukhaMg
+ * @version 1.0
+ *
+ */
+
 public class OOPsBanner {
+    public static void main(String[] args) {
+        System.out.println("oops");
+    }
 }
