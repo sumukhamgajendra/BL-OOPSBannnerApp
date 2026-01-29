09f084f965cb746f895597655f5ba504676b506e
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-01-29 17:50:07 +0530

    coded to Render OOPs banner inline array

--- src/main/java/com/bridgelabz/OOPsBanner.java
+++ src/main/java/com/bridgelabz/OOPsBanner.java
@@ -1,4 +1,26 @@
 package com.bridgelabz;
+/**
+ *
+ *
+ * @author Sumukha
+ * @version 5.0
+ */
 
 public class OOPsBanner {
+    public static void main(String[] args) {
+        String[] lines = {
+            String.join(" ", "   ***   ", "    ***    ", "******   ", "  ****** ");
+            String.join(" ", " **   ** ", "  **   **  ", "**    ** ", "**       ");
+            String.join(" ", "**     **", " **     ** ", "**    ** ", "**       ");
+            String.join(" ", "**     **", " **     ** ", "*******  ", "******* ");
+            String.join(" ", "**     **", " **     ** ", "**       ", "       **");
+            String.join(" ", " **   ** ", "  **   **  ", "**       ", "       **");
+            String.join(" ", "   ***   ", "    ***    ", "**       ", " ******* ");
+        };
+
+        for(String line : lines){
+            System.out.println(line);
+        }
+    }
 }
+
